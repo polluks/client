0fcf972f438c7f3034edfa5f94a8a08a55fbc27d
Maxwell Krohn <themax@gmail.com>
2015-11-19 10:39:35 -0500

    fixes from code review

--- go/kbcmf/decrypt.go
+++ go/kbcmf/decrypt.go
@@ -158,7 +158,7 @@ func (ds *decryptStream) processEncryptionHeader(hdr *EncryptionHeader) error {
 		return err
 	}
 
-	// Lookup the sender's secret key in our keyring, and import
+	// Lookup the sender's public key in our keyring, and import
 	// it for use. However, if the sender key is the same as the ephemeral
 	// key, then assume "anonymous mode", so use the already imported anonymous
 	// key.
